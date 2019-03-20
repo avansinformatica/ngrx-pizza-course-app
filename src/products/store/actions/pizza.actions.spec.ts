95ae3039d7ef87960ebc874e4466c5347b4c9f7d
Robin Schellius <r.schellius@avans.nl>
2019-03-20 20:39:29 +0100

    Added tests

--- src/products/store/actions/pizza.actions.spec.ts
+++ src/products/store/actions/pizza.actions.spec.ts
@@ -1,8 +1,8 @@
 import * as fromPizzas from './pizzas.action'
 
-describe('Pizzas actions', () => {
-    describe('Load Pizza Actions', () => {
-        describe('Load Pizzas', () => {
+describe('Pizzas Actions', () => {
+    describe('LoadPizzas Actions', () => {
+        describe('LoadPizzas', () => {
             it('should create an action', () => {
                 const action = new fromPizzas.LoadPizzas()
 
@@ -12,13 +12,205 @@ describe('Pizzas actions', () => {
             })
         })
 
-        describe('Load Pizzas Fail', () => {
+        describe('LoadPizzasFail', () => {
             it('should create an action', () => {
-                const payload = { message: 'Load error' }
+                const payload = { message: 'Load Error' }
                 const action = new fromPizzas.LoadPizzasFail(payload)
 
                 expect({ ...action }).toEqual({
                     type: fromPizzas.LOAD_PIZZAS_FAIL,
+                    payload,
+                })
+            })
+        })
+
+        describe('LoadPizzasSuccess', () => {
+            it('should create an action', () => {
+                const payload = [
+                    {
+                        id: 1,
+                        name: 'Pizza #1',
+                        toppings: [
+                            { id: 1, name: 'onion' },
+                            { id: 2, name: 'mushroom' },
+                            { id: 3, name: 'basil' },
+                        ],
+                    },
+                    {
+                        id: 2,
+                        name: 'Pizza #2',
+                        toppings: [
+                            { id: 1, name: 'onion' },
+                            { id: 2, name: 'mushroom' },
+                            { id: 3, name: 'basil' },
+                        ],
+                    },
+                ]
+                const action = new fromPizzas.LoadPizzasSuccess(payload)
+
+                expect({ ...action }).toEqual({
+                    type: fromPizzas.LOAD_PIZZAS_SUCCESS,
+                    payload,
+                })
+            })
+        })
+    })
+
+    describe('CreatePizza Actions', () => {
+        describe('CreatePizza', () => {
+            it('should create an action', () => {
+                const payload = {
+                    name: 'Pizza #2',
+                    toppings: [
+                        { id: 1, name: 'onion' },
+                        { id: 2, name: 'mushroom' },
+                        { id: 3, name: 'basil' },
+                    ],
+                }
+                const action = new fromPizzas.CreatePizza(payload)
+
+                expect({ ...action }).toEqual({
+                    type: fromPizzas.CREATE_PIZZA,
+                    payload,
+                })
+            })
+        })
+
+        describe('CreatePizzaFail', () => {
+            it('should create an action', () => {
+                const payload = { message: 'Create Error' }
+                const action = new fromPizzas.CreatePizzaFail(payload)
+
+                expect({ ...action }).toEqual({
+                    type: fromPizzas.CREATE_PIZZA_FAIL,
+                    payload,
+                })
+            })
+        })
+
+        describe('CreatePizzaSuccess', () => {
+            it('should create an action', () => {
+                const payload = {
+                    id: 2,
+                    name: 'Pizza #2',
+                    toppings: [
+                        { id: 1, name: 'onion' },
+                        { id: 2, name: 'mushroom' },
+                        { id: 3, name: 'basil' },
+                    ],
+                }
+                const action = new fromPizzas.CreatePizzaSuccess(payload)
+
+                expect({ ...action }).toEqual({
+                    type: fromPizzas.CREATE_PIZZA_SUCCESS,
+                    payload,
+                })
+            })
+        })
+    })
+
+    describe('UpdatePizza Actions', () => {
+        describe('UpdatePizza', () => {
+            it('should create an action', () => {
+                const payload = {
+                    id: 2,
+                    name: 'Pizza #2',
+                    toppings: [
+                        { id: 1, name: 'onion' },
+                        { id: 2, name: 'mushroom' },
+                        { id: 3, name: 'basil' },
+                    ],
+                }
+                const action = new fromPizzas.UpdatePizza(payload)
+
+                expect({ ...action }).toEqual({
+                    type: fromPizzas.UPDATE_PIZZA,
+                    payload,
+                })
+            })
+        })
+
+        describe('UpdatePizzaFail', () => {
+            it('should create an action', () => {
+                const payload = { message: 'Update Error' }
+                const action = new fromPizzas.UpdatePizzaFail(payload)
+
+                expect({ ...action }).toEqual({
+                    type: fromPizzas.UPDATE_PIZZA_FAIL,
+                    payload,
+                })
+            })
+        })
+
+        describe('UpdatePizzaSuccess', () => {
+            it('should create an action', () => {
+                const payload = {
+                    id: 2,
+                    name: 'Pizza #2',
+                    toppings: [
+                        { id: 1, name: 'onion' },
+                        { id: 2, name: 'mushroom' },
+                        { id: 3, name: 'basil' },
+                    ],
+                }
+                const action = new fromPizzas.UpdatePizzaSuccess(payload)
+
+                expect({ ...action }).toEqual({
+                    type: fromPizzas.UPDATE_PIZZA_SUCCESS,
+                    payload,
+                })
+            })
+        })
+    })
+
+    describe('DeletePizza Actions', () => {
+        describe('DeletePizza', () => {
+            it('should create an action', () => {
+                const payload = {
+                    id: 2,
+                    name: 'Pizza #2',
+                    toppings: [
+                        { id: 1, name: 'onion' },
+                        { id: 2, name: 'mushroom' },
+                        { id: 3, name: 'basil' },
+                    ],
+                }
+                const action = new fromPizzas.DeletePizza(payload)
+
+                expect({ ...action }).toEqual({
+                    type: fromPizzas.DELETE_PIZZA,
+                    payload,
+                })
+            })
+        })
+
+        describe('DeletePizzaFail', () => {
+            it('should create an action', () => {
+                const payload = { message: 'Delete Error' }
+                const action = new fromPizzas.DeletePizzaFail(payload)
+
+                expect({ ...action }).toEqual({
+                    type: fromPizzas.DELETE_PIZZA_FAIL,
+                    payload,
+                })
+            })
+        })
+
+        describe('DeletePizzaSuccess', () => {
+            it('should create an action', () => {
+                const payload = {
+                    id: 2,
+                    name: 'Pizza #2',
+                    toppings: [
+                        { id: 1, name: 'onion' },
+                        { id: 2, name: 'mushroom' },
+                        { id: 3, name: 'basil' },
+                    ],
+                }
+                const action = new fromPizzas.DeletePizzaSuccess(payload)
+
+                expect({ ...action }).toEqual({
+                    type: fromPizzas.DELETE_PIZZA_SUCCESS,
                     payload,
                 })
             })
